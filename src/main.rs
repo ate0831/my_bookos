1a6dba0edcc88115821215095220518fc62b624d
atte0831 <ateateatte@gmail.com>
2023-10-07 17:52:11 +0900

    add link script

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 #![no_main]
 #![no_std]
 
+// The reset vector, a pointer into the reset handler
+#[link_section = ".vector_table.reset_vector"]
+#[no_mangle]
+pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 use core::panic::PanicInfo;
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    let x = 42;
+
+    //無限ループにして値を返さない
+    loop{}
+}
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
