dd540a8c1fe1c0b0404b3be42fd7fe3c85dec303
atte0831 <ateateatte@gmail.com>
2023-10-08 19:24:49 +0900

    implement SysTick handler

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,54 @@ pub unsafe extern "C" fn Reset() -> ! {
     loop {}
 }
 
-// The reset vector, a pointer into the reset handler
+// The reset vector, a pointer into the reset panic_handler
 #[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
 pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
+pub union Vector {
+    reserved: u32,
+    handler: unsafe extern "C" fn(),
+}
+
+extern "C" {
+    fn NMI();
+    fn HardFault();
+    fn MemManage();
+    fn BusFault();
+    fn UsageFault();
+    fn SVCall();
+    fn PendSV();
+    fn SysTick();
+}
+
+#[link_section = ".vector_table.exceptions"]
+#[no_mangle]
+pub static EXCEPTIONS: [Vector; 14] = [
+    Vector { handler: NMI },
+    Vector { handler: HardFault },
+    Vector { handler: MemManage },
+    Vector { handler: BusFault },
+    Vector {
+        handler: UsageFault,
+    },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: SVCall },
+    Vector { reserved: 0 },
+    Vector { reserved: 0 },
+    Vector { handler: PendSV },
+    Vector { handler: SysTick },
+];
+
+#[no_mangle]
+pub extern "C" fn DefaultExceptionHandler() {
     loop {}
 }
