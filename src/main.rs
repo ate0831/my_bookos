f7336ee315d533f1697c474b9dacea2cfa1fa460
atte0831 <ateateatte@gmail.com>
2023-10-08 13:42:32 +0900

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 #![no_main]
 #![no_std]
 
+use core::panic::PanicInfo;
+use core::ptr;
+use cortex_m_semihosting::hprintln;
+
+#[no_mangle]
+pub unsafe extern "C" fn Reset() -> ! {
+    extern "C" {
+        static mut _sbss: u8;
+        static mut _ebss: u8;
+        static mut _sidata: u8;
+        static mut _sdata: u8;
+        static mut _edata: u8;
+    }
+
+    let count = &_ebss as *const u8 as useize - &_sbss as *const usize;
+    prt::write_bytes(&mut _sbss as *mut u8, 0, count);
+
+    let count = &_edata as *const u8 as usize - &_sdata as *const u8 as usize;
+    ptr::copy_nonoverlapping(&_sidata as *const u8, &mut _sdata as *mut u8, count);
+
+    hprintln!("Hello world").unwarp();
+
+    //無限ループにして値を返さない
+    loop {}
+}
+
 // The reset vector, a pointer into the reset handler
 #[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
 pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
-use core::panic::PanicInfo;
-
-#[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
-    let _x = 42;
-
-    //無限ループにして値を返さない
-    loop{}
-}
-
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop{}
+    loop {}
 }
